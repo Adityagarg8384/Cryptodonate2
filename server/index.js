f41d00030f01bf6dfe3ff5a004f49ecc8ec6d220
Adityagarg8384 <adityagarg8384@gmail.com>
2024-03-22 23:50:09 +0530

    addfiles

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const bodyParser= require("body-parser");
 const twilio= require("twilio");
 const mongoose= require("mongoose");
 const cors= require("cors");
-require("dotenv").config();
+
 
 const app= express();
 const PORT= 3001;
